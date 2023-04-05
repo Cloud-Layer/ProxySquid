9b600def041a8c74532118d578031286350c647c
Diab Awada <diabawada@live.com>
2023-04-05 03:30:26 +0300

    fix sql

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-mysql -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < /etc/squid/squid_users.sql
+mysql -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < /etc/squid/squid_users
 /usr/sbin/squid3 -NYCd 1
