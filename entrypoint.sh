681adc66023b787302d4093bec4448c41767d0f5
Diab Awada <diabawada@live.com>
2023-04-06 12:06:30 +0300

    dqw

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
-mysql -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < /etc/squid/squid_users
+#mysql -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < /etc/squid/squid_users
+mysql -h mysql -u squid_user -psquid_password squid_db < /etc/squid/squid_users
 /usr/sbin/squid -NYCd 1
