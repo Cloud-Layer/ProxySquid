ccbdaf5569f0603af7b444596e9df8e590f6f5ef
Diab Awada <diabawada@live.com>
2023-04-08 13:09:41 +0300

    qwdqwd

--- mysql_auth.py
+++ mysql_auth.py
@@ -3,24 +3,26 @@
 use DBI;
 use Digest::MD5 qw(md5_hex);
 
-# Replace these values with your MySQL database credentials and DSN string
+# Replace these values with your MySQL database credentials and SQL query
 my $db_user = "squid_user";
 my $db_pass = "squid_password";
-my $dsn = "DBI:mysql:database=squid_db;host=mysql";
+my $db_name = "squid_db";
+my $db_host = "mysql";
+my $sql_query = "SELECT password FROM squid_users WHERE username=?";
 
-# Get the username and password from Squid
 my $username = lc <STDIN>;
 chomp $username;
 
 my $password = lc <STDIN>;
 chomp $password;
 
-# Connect to the MySQL database using the DSN string
+# Connect to the MySQL database
+my $dsn = "DBI:mysql:database=$db_name;host=$db_host";
 my $dbh = DBI->connect($dsn, $db_user, $db_pass)
   or die "Can't connect to database: " . DBI->errstr;
 
 # Prepare and execute the SQL query
-my $sth = $dbh->prepare("SELECT password FROM users WHERE username=?");
+my $sth = $dbh->prepare($sql_query);
 $sth->execute($username);
 my ($db_password) = $sth->fetchrow_array();
 
