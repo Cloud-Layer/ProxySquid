ef6d986e32aca2ebcb1c6df7a9d331752563311e
Diab Awada <diabawada@live.com>
2023-04-08 13:00:23 +0300

    dqwd

--- mysql_auth.py
+++ mysql_auth.py
@@ -1,48 +1,38 @@
-#!/usr/bin/env python3
+#!/usr/bin/perl
 
-import sys
-import mysql.connector
-import logging
+use DBI;
+use Digest::MD5 qw(md5_hex);
 
-# Set up logging
-logging.basicConfig(level=logging.DEBUG)
+# Replace these values with your MySQL database credentials and DSN string
+my $db_user = "squid_user";
+my $db_pass = "squid_password";
+my $dsn = "DBI:mysql:database=squid_db;host=mysql";
 
-# MySQL connection information
-host = "mysql"
-user = "squid_user"
-password = "squid_password"
-database = "squid_db"
+# Get the username and password from Squid
+my $username = lc <STDIN>;
+chomp $username;
 
-# Get username and password from Squid
-# line = sys.stdin.readline().strip()
-line = input().strip()
-username, password = line.split()
+my $password = lc <STDIN>;
+chomp $password;
 
-# Connect to MySQL database
-logging.debug("Connecting to database...")
-cnx = mysql.connector.connect(host=host, user=user, password=password, database=database)
-cursor = cnx.cursor()
+# Connect to the MySQL database using the DSN string
+my $dbh = DBI->connect($dsn, $db_user, $db_pass)
+  or die "Can't connect to database: " . DBI->errstr;
 
-# Check if user is authenticated
-logging.debug("Checking user authentication...")
-query = "SELECT * FROM squid_users WHERE username=%s AND password=%s"
-cursor.execute(query, (username, password))
-result = cursor.fetchone()
+# Prepare and execute the SQL query
+my $sth = $dbh->prepare("SELECT password FROM users WHERE username=?");
+$sth->execute($username);
+my ($db_password) = $sth->fetchrow_array();
 
-# fetch the result set
-result = cursor.fetchone()
-for row in result:
-    print(row)
+# Compare the hashed passwords
+my $hashed_password = md5_hex("$username:$db_password:$password");
+my $is_authenticated = $hashed_password eq $db_password;
 
-if result is not None:
-    # User is authenticated
-    logging.debug("User authenticated: %s", username)
-    print("OK")
-else:
-    # User is not authenticated
-    logging.debug("User authentication failed: %s", username)
-    print("ERR")
+if ($is_authenticated) {
+  print "OK\n";
+} else {
+  print "ERR\n";
+}
 
-# close the cursor and connection
-cursor.close()
-cnx.close()
+# Disconnect from the database
+$dbh->disconnect();
